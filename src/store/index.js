f1c177e81eb6f4bf0fa532707aed0d75f24db138
user name <99411978+Symwalker@users.noreply.github.com>
2023-02-28 13:20:10 +0500

    create action

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+export const increment = () => {
+  return{
+    type : "INCREMENT",
+    // TYPE KE SATH OR KUCH BHI DE SAKTAY HO
+  }
+}
+export const decrement = () => {
+  return{
+    type : "DECREMENT",
+    // TYPE KE SATH OR KUCH BHI DE SAKTAY HO
+  }
+}
+
